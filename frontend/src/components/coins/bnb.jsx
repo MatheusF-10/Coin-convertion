d4358d70e0b7744d5c258c975d6fcdc23c825646
MatheusF-10 <matheus.souza3472@gmail.com>
2023-07-15 17:39:03 -0300

    feat: incomplete front and fix back

---
+++ frontend/src/components/coins/bnb.jsx
@@ -0,0 +1,8 @@
+import BnbImage from '@/assets/bnb.png'
+export const Bnb = () => {
+  return(
+    <div>
+      <img src={BnbImage} alt="Imagem do bnb" className='image-coin'/>
+    </div>
+  )
+}
---
+++ frontend/src/components/coins/tether.jsx
@@ -0,0 +1,8 @@
+import TetherImage from '@/assets/tether.png'
+export const Tether = () => {
+  return(
+    <div>
+      <img src={TetherImage} alt="Imagem do tether" className='image-coin'/>
+    </div>
+  )
+}

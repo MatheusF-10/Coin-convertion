9eaae9ebe83aa3897c73d4665e82422821cf9f0a
Porto <rodrigoandradeporto@hotmail.com>
2023-07-15 17:20:20 -0300

    refactor: update coins route

--- backend/api.py
+++ backend/api.py
@@ -1,18 +1,15 @@
 from flask import Flask
 from pycoingecko import CoinGeckoAPI
 
-from backend.utils import crypto_coin_list, coin_currencies, error_message
+from backend.utils import crypto_coin_list, coin_currencies
 
 app = Flask(__name__)
 cg = CoinGeckoAPI()
 
 
-@app.route('/price/<coin>', methods=['GET'])
-def get_price(coin):
-    if coin in crypto_coin_list:
-        return cg.get_price(coin, coin_currencies)
-    else:
-        return error_message['invalid_coin']
+@app.route('/coins', methods=['GET'])
+def get_coins():
+    return cg.get_price(crypto_coin_list, coin_currencies)
 
 
 if __name__ == '__main__':

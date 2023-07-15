30c5d9da6600dbd5bf92b57ded6bcc5eb6d6779c
Porto <rodrigoandradeporto@hotmail.com>
2023-07-15 16:37:00 -0300

    feat: add route price query

--- backend/api.py
+++ backend/api.py
@@ -1,12 +1,19 @@
 from flask import Flask
+from pycoingecko import CoinGeckoAPI
+
+from backend.utils import crypto_coin_list, coin_currencies, error_message
 
 app = Flask(__name__)
+cg = CoinGeckoAPI()
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route('/price/<coin>', methods=['GET'])
+def get_price(coin):
+    if coin in crypto_coin_list:
+        return cg.get_price(coin, coin_currencies)
+    else:
+        return error_message['invalid_coin']
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ backend/utils.py
@@ -0,0 +1,5 @@
+crypto_coin_list = ['binancecoin', 'bitcoin', 'ethereum', 'tether']
+coin_currencies = ['usd', 'brl', 'eur']
+error_message = dict(
+    invalid_coin="Invalid coin name"
+)
